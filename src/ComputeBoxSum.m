ceb435d624574b04583c54d4554ba92334dbbbb7
Mattias Lindström <mattili@kth.se>
2012-04-20 12:04:40 +0200

    70% of Task 1

---
+++ src/ComputeBoxSum.m
@@ -0,0 +1,13 @@
+function A = ComputeBoxSum(ii_im, x, y, w, h)
+    if x == 1 && y == 1
+         A = ii_im(y+h-1, x+w-1);
+    elseif x == 1 
+         A = ii_im(y+h-1, x+w-1)  - ii_im(y-1, x+w-1);    
+    elseif y == 1
+         A = ii_im(y+h-1, x+w-1) - ii_im(y+h-1, x-1);
+    else
+         A = ii_im(y+h-1, x+w-1) - ii_im(y+h-1, x-1) - ii_im(y-1, x+w-1) + ii_im(y-1, x-1);
+    end
+        
+    
+end
